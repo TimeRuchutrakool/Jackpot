d8e0b049566b37949bbf18c0c1042092d2173b23
tharmJaa <timeruchu0808@gmail.com>
2023-03-25 20:45:58 +0700

    Initial Commit

---
+++ JACKPOT/JACKPOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  JACKPOTApp.swift
+//  JACKPOT
+//
+//  Created by Time Ruchutrakool on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct JACKPOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
